c7c328f2bbbf3941a2f22b59ce37d285a4ba59b2
Aref_Keshavarz <Arefkeshavarz1220@gmail.com>
2023-11-08 00:19:40 +0330

    1. add calculate_the_answer function .

--- Operations.py
+++ Operations.py
@@ -1,3 +1,8 @@
+import os
+
+import Arts
+
+
 def sum(base_amount, second_amount):
     """ this function return sum of inputs that is two float number """
     return base_amount + second_amount
@@ -13,6 +18,41 @@ def multiplication(base_amount, second_amount):
     return base_amount * second_amount
 
 
-def Division(base_amount, second_amount):
+def division(base_amount, second_amount):
     """ this function return Division of inputs that is two float number"""
     return base_amount / second_amount
+
+
+def calculate_the_answer(base_amount):
+    application_status = "1"
+    second_amount = float(input(" please enter second amount : "))
+    while 1:
+        operation = input(" please enter your operation ( + - * / ) : ")
+        if operation == "+":
+            base_amount = sum(base_amount, second_amount)
+            break
+        elif operation == "-":
+            base_amount = subtraction(base_amount, second_amount)
+            break
+        elif operation == "*":
+            base_amount = multiplication(base_amount, second_amount)
+            break
+        elif operation == "/":
+            base_amount = division(base_amount, second_amount)
+            break
+        else:
+            print(" *** wrong input . ")
+    print("-------- result --------")
+    print(f" >>> {base_amount}")
+    application_status = input(
+        f" >> type \"1\" to continue with {base_amount} , type \"2\" to start a new calculation , type \"3\" for exit: ")
+    if application_status == "1":
+        calculate_the_answer(base_amount)
+    elif application_status == "3":
+        exit(1)
+    elif application_status == "2":
+        os.system('cls')
+        print(Arts.calculator_ascii_art)
+        print(" ---- welcome to calculator ---- ")
+        base_amount = float(input(" please enter number 1 : "))
+        calculate_the_answer(base_amount)
--- main.py
+++ main.py
@@ -4,22 +4,4 @@ import Operations
 print(Arts.calculator_ascii_art)
 print(" ---- welcome to calculator ---- ")
 base_amount = float(input(" please enter number 1 : "))
-application_status = "1"
-while (application_status == "1"):
-    second_amount = float(input(" please enter second amount : "))
-    while 1:
-        operation = input(" please enter your choose ( + - * / ) : \n >>>")
-        if operation == "+":
-            base_amount = Operations.sum(base_amount, second_amount)
-            break
-        elif operation == "-":
-            base_amount = Operations.subtraction(base_amount, second_amount)
-            break
-        elif operation == "*":
-            base_amount = Operations.multiplication(base_amount, second_amount)
-            break
-        elif operation == "/":
-            base_amount = Operations.Division(base_amount, second_amount)
-            break
-        else:
-            print(" *** wrong input . ")
+Operations.calculate_the_answer(base_amount)
