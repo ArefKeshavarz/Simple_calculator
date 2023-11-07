21de57754859a52e6ef30a2b95dc5663f3b58184
Aref_Keshavarz <Arefkeshavarz1220@gmail.com>
2023-11-07 22:37:41 +0330

    add subtraction function .

--- Operations.py
+++ Operations.py
@@ -3,3 +3,9 @@ def sum(base_amount, second_amount):
     return base_amount + second_amount
 
 
+def subtraction(base_amount, second_amount):
+    """ this function return subtraction of inputs that is two float number """
+    return base_amount - second_amount
+
+
+
